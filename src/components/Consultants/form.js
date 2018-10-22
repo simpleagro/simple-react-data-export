c3062e3d581777102246cac8e3e0c4200ea2b73b
Vinicius Felix <viniciusfelix.exe@gmail.com>
2018-10-22 10:35:24 -0300

    Corrigido campo de gerente

--- src/components/Consultants/form.js
+++ src/components/Consultants/form.js
@@ -305,7 +305,7 @@ class ConsultantForm extends Component {
             )}
           </Form.Item>
 
-          {this.state.formData.cargo !== "GERENTE" && <Form.Item label="Gerente" {...formItemLayout}>
+          <Form.Item label="Gerente" {...formItemLayout}>
             {getFieldDecorator("gerente_id", {
               rules: [{ required: this.state.formData.cargo === "GERENTE" ? false : true, message: "Este campo é obrigatório!" }],
               initialValue: this.state.formData.cargo === "GERENTE" ? null : this.state.formData.gerente_id
@@ -330,7 +330,7 @@ class ConsultantForm extends Component {
                 }
               </Select>
             )}
-          </Form.Item>}
+          </Form.Item>
 
           <Form.Item label="Tipo" {...formItemLayout}>
             {getFieldDecorator("tipo", {
