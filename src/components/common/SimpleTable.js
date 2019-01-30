e061ec4f8b76a617002534aabe71596ae32bcea1
Simple Agro\Wendell <wendell.guimaraes@simpleagro.com.br>
2019-01-30 17:02:05 -0200

    Voltando ao padrão anterior do table scroll

--- src/components/common/SimpleTable.js
+++ src/components/common/SimpleTable.js
@@ -1,42 +1,10 @@
 import React from "react";
-import { Table, Spin, Pagination, Button } from "antd";
+import { Table, Spin } from "antd";
 
 const SimpleTable = ({ spinning, ...props }) => {
-
-
-  props.columns = props.columns.map((c,index) => {
-    if (c.dataIndex === "action") {
-      c.width = c.width || 150;
-      c.fixed = c.fixed || "right";
-    } else {
-      if (c.hasOwnProperty("fixed")) c.width = c.width || 100;
-    }
-
-    return c;
-  });
-
   return (
     <Spin spinning={spinning} tip="Carregando..." size="large">
-      {/* <Button
-      ghost
-      type="primary"
-      icon="reload"
-      onClick={() => window.location.reload()}>
-      Atualizar
-    </Button> */}
-      <Table
-        {...{
-          scroll: {
-            x:
-            document.querySelector(".ant-layout-content").offsetWidth +
-              props.columns.reduce(function(prev, elem) {
-                return prev + (elem.hasOwnProperty("fixed") && elem.width);
-              }, 0)
-          },
-          ...props
-        }}
-      />
-      {/* scroll={{ y: 240 }} */}
+      <Table {...{ scroll: { x: true }, ...props }} />
     </Spin>
   );
 };
