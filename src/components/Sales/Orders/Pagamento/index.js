321c268376bfa6f62de645c248376f7d57d4a472
Simple Agro\Wendell <wendell.guimaraes@simpleagro.com.br>
2019-03-14 17:04:09 -0300

    Corrige mudança de status do pedido quando finalizar

--- src/components/Sales/Orders/Pagamento/index.js
+++ src/components/Sales/Orders/Pagamento/index.js
@@ -18,6 +18,8 @@ import CalculoPagamentoGraos from "./partials/CalculoPagamentoGraos";
 import ParcelasManual from "./partials/ParcelasManual";
 import ParcelasAutomaticas from "./partials/ParcelasAutomaticas";
 
+const STATUS_CONCLUIR_PEDIDO = "Aguardando Aprovação";
+
 class OrderPaymentForm extends Component {
   constructor(props) {
     super(props);
@@ -162,7 +164,8 @@ class OrderPaymentForm extends Component {
       try {
         await OrderPaymentService.update(this.props.pedido._id)({
           ...this.state.formData,
-          ...this.props.pedido.pagamento
+          ...this.props.pedido.pagamento,
+          status: STATUS_CONCLUIR_PEDIDO
         });
         flashWithSuccess(
           `Pedido #${this.props.pedido.numero} finalizado com sucesso!`
