1bf97f57b2e766f780b68acd0d2da357786e5084
Jessika Machado Pereira <jessika.pereira@grupotecagro.com>
2019-03-28 17:43:09 -0300

    Tela Aprovação de Pedidos

---
+++ src/config/Menus/aprovacao-pedidos.js
@@ -0,0 +1,19 @@
+import SimpleLoadable from "../../components/common/SimpleLoadable";
+
+const menus = {
+  "/aprovacao": {
+    component: SimpleLoadable(() => import("../../components/Sales/Approval/DadosBasicos/index")),
+    path: "/aprovacao",
+    label: "Aprovação Pedidos",
+    icon: "check-square",
+    showMenu: true,
+    rule: 'Indicator'
+  }/* ,
+  "/aprovacao/:id/edit": {
+    component: SimpleLoadable(() => import("../../components/Sales/Orders/DadosBasicos/form")),
+    path: "/pedidos/:id/edit",
+    showMenu: false
+  } */
+};
+
+export default menus;
