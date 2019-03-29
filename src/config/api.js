6ebf9e62895dea7a542e11ec49ce84d69a09e341
Simple Agro\Wendell <wendell.guimaraes@simpleagro.com.br>
2019-03-29 17:02:19 -0300

    revert commit

--- src/config/api.js
+++ src/config/api.js
@@ -9,8 +9,6 @@ API_URL = `${process.env.REACT_APP_API_IP.replace(
   ""
 )}:${process.env.REACT_APP_API_PORT.replace(/\s/g, "")}/api`;
 
-API_URL = `https://homologacao.simpleagro.com.br/api`
-
 const cookies = new Cookies();
 
 const baseApi = axios.create({
