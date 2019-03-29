7198d6474558481bfa00d319342e6d28f69bbe02
Vinicius Felix <viniciusfelix.exe@gmail.com>
2019-03-29 13:48:50 -0300

    Tabela de preço pdf

---
+++ src/services/productgroups.mobile.js
@@ -0,0 +1,27 @@
+import { baseApi as api } from "../config/api";
+
+const baseURL = "/productgroups.mobile";
+
+export const list = aqp => {
+  return api
+    .get(baseURL, {
+      params: aqp
+    })
+    .then(response => response.data)
+};
+
+export const get = _id =>
+  api.get(`${baseURL}/${_id}`).then(response => response.data);
+
+export const create = obj =>
+  api.post(baseURL, obj).then(response => response.data);
+
+export const update = obj =>
+  api.put(`${baseURL}/${obj._id}`, obj).then(response => response.data);
+
+export const remove = _id =>
+  api.delete(`${baseURL}/${_id}`).then(response => response.data);
+
+export const changeStatus = (_id, status) => {
+  api.put(`${baseURL}/${_id}`, { status }).then(response => response.data);
+};
